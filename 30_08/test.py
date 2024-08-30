2af564b75fa73d3b23ee9d6b44cd94da5a2c9fa4
GianfriShow <131198417+GianfriShow@users.noreply.github.com>
2024-08-30 15:51:39 +0200

    Add files via upload

--- 30_08/test.py
+++ 30_08/test.py
@@ -1,3 +1,4 @@
+# 5
 # take as input a word and add it to a list, ask if the user wants to repeat this by adding another word, then print all elements in the list at the end
 
 def print_list():
@@ -22,3 +23,15 @@ def print_list():
         print(element)
 
 print_list()
+
+# 7
+'''
+Le tre regole fondamentali dell'OOP sono incapsulamento, ereditarieta', e polimorfismo:
+l'incapsulamento si riferisce alla capacita' di poter nascondere certi aspetti/elementi degli oggetti che non vogliamo far trapelare,
+o al contrario di mostrare cio' che desideriamo rivelare;
+l'ereditarieta' e' la possibilita' di assegnare a vari oggetti le caratteristiche specifiche di un'altra classe gia' esistente, permettendo
+in questo modo di creare delle gerarchie o alberi genealogici con relazioni di parent-child;
+il polimorfismo e' strettamente collegato all'ereditarieta' in quanto e' l'abilita' di cambiare forma di un oggetto ma non il comportamento,
+o al contrario di cambiare il comportamento ma non la forma, permettendoci di trattare oggetti di classi diverse allo stesso modo fintanto che abbiano
+una classe in comune (stesso parent).
+'''
