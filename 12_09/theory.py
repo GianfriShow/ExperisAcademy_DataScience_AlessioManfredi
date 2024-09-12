ac3359195060a69abebb1f1e40e762a396894bf9
GianfriShow <131198417+GianfriShow@users.noreply.github.com>
2024-09-12 18:10:18 +0200

    Add files via upload

--- 12_09/theory.py
+++ 12_09/theory.py
@@ -72,3 +72,87 @@ with open('12_09/binary.bin','rb') as my_file:  # read a binary file
 print(content)  # unreadable because binary
 readable_content = pickle.loads(content)  # make it readable
 print(readable_content)
+
+
+
+class Cat:
+
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+    
+    def __str__(self):  # dunder method (characterized by the '__ __')
+        return f'Name: {self.name}, Type: {self.type}'  # every time the object is treated as a string, this will be returned
+
+
+kitty = Cat('cutie','orange')
+print(kitty)  # prints the object info and its location if no __str__ method, otherwise it runs the latter
+    
+
+
+# connect to databases
+
+import mysql.connector
+
+mydb = mysql.connector.connect(  # connect to the database
+    host = 'localhost',
+    user = 'root',
+    password = 'root'
+#   port = ....  # in case we change port from the default
+)
+
+print(mydb)
+
+mycursor = mydb.cursor()  # interact with the database through the cursor
+
+sql = 'create database python_exercise'  # prepare sql query
+mycursor.execute(sql)  # execute query
+
+sql = 'show databases'
+mycursor.execute(sql)
+
+for db in mycursor:  # the cursor is an iterable with the database names
+    print(f'Database: {db}')
+for tb in mycursor:
+    print(f'Table: {tb}')
+
+
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'root',
+    database = 'python_exercise'  # this time we specify the database we work in
+)
+mycursor = mydb.cursor()
+sql = 'create table users(id int auto_increment primary key, name varchar(255), surname varchar(255))'
+mycursor.execute(sql)
+
+sql = 'insert into users(name,surname) values(%s,%s)'
+val = [("Mark","Jones"),("Luke","Stone"),("John","Rovers")]
+mycursor.executemany(sql, val)  # if we only have one tuple to add, we use execute() instead
+
+mydb.commit()
+
+print(mycursor.rowcount,"data points uploaded")
+print(mycursor.lastrowid)  # show the id of the last row
+
+
+sql = 'select * from users where id>1'
+mycursor.execute(sql)
+
+data = mycursor.fetchall()  # fetchall() retrieves all the output rows of the query, exhausting them from memory
+for datum in data:
+    print(datum)
+
+sql = 'select * from users where id>1'
+mycursor.execute(sql)
+
+datum = mycursor.fetchone()  # if we hadn't rerun the query, we wouldn't have been able to fetch anything (would have returned None)
+print(datum)  # fetchone() only returns the first row of the query output
+for element in datum:  # therefore, we now extract all variables of the first row only
+    print(element)
+
+sql = 'delete from users where id = 1'
+mycursor.execute(sql)
+mydb.commit()
+print(mycursor.rowcount)
